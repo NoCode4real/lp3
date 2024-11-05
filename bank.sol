f752b4c3ab19d34fa572887259c1f3bf294a845b
Kalpraj Shejwal <127539273+NoCode4real@users.noreply.github.com>
2024-11-05 22:25:53 +0530

    bank.sol

--- bank.sol
+++ bank.sol
@@ -12,19 +12,19 @@ contract Bank {
 
     function withdraw()public payable{
         require(balance > 0,"you broke");
-        require(msg.sender==accHolder,"bhag bsdk");
+        require(msg.sender==accHolder,"John Doe");
         payable(msg.sender).transfer(balance);
         balance=0;
     }
 
     function deposit() public payable{
         require(msg.value > 0,"Deposit amt grt than 0");
-        require(msg.sender==accHolder,"bhag bsdk");
+        require(msg.sender==accHolder,"John Doe");
         balance += msg.value;
     }
 
     function showBalance()public view returns(uint){
-        require(msg.sender==accHolder,"bhag bsdk");
+        require(msg.sender==accHolder,"John Doe");
         return balance;
     }
 }
